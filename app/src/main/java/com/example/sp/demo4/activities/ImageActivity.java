55af0b9cc8fbd2d36b68bd56178b9df98153c032
Weihuanming <chaoswar@163.com>
2016-08-05 18:52:57 +0800

    'photoview应用'

--- app/src/main/java/com/example/sp/demo4/activities/ImageActivity.java
+++ app/src/main/java/com/example/sp/demo4/activities/ImageActivity.java
@@ -5,26 +5,20 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
 import com.example.sp.demo4.R;
 import com.example.sp.demo4.adapters.RecyclerAdapter;
 import com.example.sp.demo4.adapters.ViewpageAdapter;
 import com.example.sp.demo4.utils.FileUtils;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ImageActivity extends AppCompatActivity{
 
     private RecyclerAdapter recyclerAdapter;
     private Toolbar toolbar;
     private ViewPager viewPager;
-    private List<View> list_view;
     private ViewpageAdapter adapter;
+    final Context context=this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,22 +37,15 @@ public class ImageActivity extends AppCompatActivity{
     }
 
     private void iniViewPager(){
-        viewPager=(ViewPager)findViewById(R.id.viewPager);
+        viewPager=(ViewPager)findViewById(R.id.pager);
+
         recyclerAdapter=new RecyclerAdapter(this);
-        final Context context=this;
         recyclerAdapter.addAll(FileUtils.getImageLibrary(context));
-        list_view=new ArrayList<>();
-        for(int i=0;i<recyclerAdapter.getItemCount();i++){
 
-            View view=LayoutInflater.from(this).inflate(R.layout.image_view,null);
-            ImageView imageView=(ImageView)view.findViewById(R.id.image_view);
-
-            Glide.with(this).load(recyclerAdapter.get(i)).fitCenter().into(imageView);
-            list_view.add(view);
-        }
-        adapter=new ViewpageAdapter(list_view);
+        adapter=new ViewpageAdapter(this,recyclerAdapter);
         viewPager.setAdapter(adapter);
         Bundle bundle = getIntent().getExtras();
+
         viewPager.setCurrentItem(bundle.getInt("which"));
         viewPager.setPageMargin(35);
     }
--- app/src/main/java/com/example/sp/demo4/adapters/ViewpageAdapter.java
+++ app/src/main/java/com/example/sp/demo4/adapters/ViewpageAdapter.java
@@ -1,21 +1,32 @@
 package com.example.sp.demo4.adapters;
 
+import android.content.Context;
 import android.support.v4.view.PagerAdapter;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bm.library.PhotoView;
+import com.bumptech.glide.Glide;
+import com.example.sp.demo4.R;
+import com.example.sp.demo4.activities.ImageActivity;
+import com.example.sp.demo4.utils.FileUtils;
 
 import java.util.List;
 
 public class ViewpageAdapter extends PagerAdapter {
-    private List<View> list_view;
 
-    public ViewpageAdapter(List<View> list_view){
-        this.list_view=list_view;
+    private RecyclerAdapter recyclerAdapter;
+    private Context context;
+
+    public ViewpageAdapter(Context context,RecyclerAdapter recyclerAdapter){
+        this.context=context;
+        this.recyclerAdapter=recyclerAdapter;
     }
 
     @Override
     public int getCount() {
-        return list_view.size();
+        return recyclerAdapter.getItemCount();
     }
 
     @Override
@@ -25,12 +36,16 @@ public class ViewpageAdapter extends PagerAdapter {
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
-        container.addView(list_view.get(position));
-        return list_view.get(position);
+        PhotoView view=new PhotoView(context);
+        view.enable();
+        view.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+        Glide.with(context).load(recyclerAdapter.get(position)).centerCrop().into(view);
+        container.addView(view);
+        return view;
     }
 
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
-        container.removeView(list_view.get(position));
+        container.removeView((View)object);
     }
 }
